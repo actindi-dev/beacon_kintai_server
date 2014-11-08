9ede14d7b980f643517bed96c9adae73a5da6796
jiikko <n905i.1214@gmail.com>
2014-11-08 12:49:59 +0900

    ٩( 'ω' )و

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root 'top#index'
+  root 'users#index'
+
+  resources :users, only: %i(index show)
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+users_hash = [
+  { name: 'さかと', email: 'sakato@sakato.com' },
+  { name: 'オクトキャット', email: 'okuto@kyatto.com' }
+]
+
+users_hash.each { |x|
+  User.create! do |user|
+    user.name = x[:name]
+    user.email = x[:email]
+  end
+}
+
+puts 'finished!'
