4638b0e659f52adacd90985467f7e1ee5b89d54e
jiikko <n905i.1214@gmail.com>
2014-11-09 18:24:38 +0900

    ٩( 'ω' )و

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @users }
+    end
   end
 end
