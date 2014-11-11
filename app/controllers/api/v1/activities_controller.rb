ed35e4d622b9d56311fccb51c30912c44cccabc6
jiikko <n905i.1214@gmail.com>
2014-11-11 14:48:42 +0900

    (☝◞‸◟)☝

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ActivitiesController < Api::V1::ApiController
   def create
-    @user = User.find(params[:user_id])
+    @user = User.find_by(id: params[:email], password: params[:password])
     @working_day = @user.working_days.find_or_create_by(worked_on: params[:working_day_id])
     @activity = @working_day.activities.build(activated_at: DateTime.now)
     if @activity.save
